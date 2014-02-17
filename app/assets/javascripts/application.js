cbbbe6409536b8d5b1c0ae6149c05e834856d9ca
HB DEV <hbdev012@phptalent.com>
2014-02-17 12:00:12 +0530

    in it folder

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+
+
+//= require admin/jquery-1.8.3.min
+//= require jquery_ujs
+//= require admin/breakpoints
+//= require admin/jquery-ui-1.10.1.custom.min
+//= require admin/jquery.slimscroll.min
+//= require admin/bootstrap.min
+//= require admin/jquery.blockui
+//= require admin/jquery.cookie
+//= require admin/jquery.vmap
+//= require admin/jquery.vmap.russia
+//= require admin/jquery.vmap.world
+//= require admin/jquery.vmap.europe
+//= require admin/jquery.vmap.germany
+//= require admin/jquery.vmap.usa
+//= require admin/jquery.vmap.sampledata
+//= require admin/jquery.uniform.min
+//= require admin/app
+//= require foundation
+//= require_tree .
+
+$(function(){ $(document).foundation(); });
