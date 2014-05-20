0309c129498882c86d420bc3309c749726ab01c9
Gaurang Bhatt <gb@spaceotechnologies.com>
2014-05-20 12:25:33 +0530

    checkout flash messages

--- app/controllers/admin/history/orders_controller.rb
+++ app/controllers/admin/history/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::History::OrdersController < Admin::BaseController
   # GET /admin/history/orders
-  def index    
+  def index
     if current_user.designer?
       product_ids = current_user.products.map(&:id)
       @orders = Order.joins(:order_items,"LEFT JOIN variants ON variants.id = order_items.variant_id").find_finished_order_grid(params).paginate(:page => pagination_page, :per_page => pagination_rows).where("variants.product_id IN (?)",product_ids).group("orders.id")
@@ -11,20 +11,20 @@ class Admin::History::OrdersController < Admin::BaseController
 
   # GET /admin/history/orders/1
   def show
-    # if current_user.designer?
-    #   #@designer_products = product.where(["user_id=?",current_user.id]).map { |e| e.id  }
-    #   @order = Order.includes([:ship_address, :invoices,
-    #                          {:shipments => :shipping_method},
-    #                          {:order_items => [
-    #                                             {:variant => [:product, :variant_properties]}]
-    #                           }]).find_by_number(params[:id])
-    # else
+    if current_user.designer?      
+      product_ids = current_user.products.map(&:id)      
+      @order = Order.includes([:ship_address, :invoices,
+                             {:shipments => :shipping_method},
+                             {:order_items => [
+                                                {:variant => [:product, :variant_properties]}]
+                              }]).where("products.id IN (?)",product_ids).find_by_number(params[:id])
+    else
       @order = Order.includes([:ship_address, :invoices,
                              {:shipments => :shipping_method},
                              {:order_items => [
                                                 {:variant => [:product, :variant_properties]}]
                               }]).find_by_number(params[:id])
-    #end
+    end
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -63,7 +63,7 @@ class ProductsController < ApplicationController
     begin
       if params[:parent] || params[:parent]=="true"
         @cat = ProductType.find(params[:id])      
-        @products = Product.aactive.paginate(:page => pagination_page, :per_page => pagination_rows).where(["product_type_id IN (?)",@cat.child_ids]) if @cat.present?
+        @products = Product.aactive.paginate(:page => pagination_page, :per_page => pagination_rows).where(["product_type_id IN (?)",[@cat.id]+@cat.child_ids]) if @cat.present?
       else      
         @cat = ProductType.find(params[:id])
         @products = Product.aactive.paginate(:page => pagination_page, :per_page => pagination_rows).where(["product_type_id =?",params[:id]])
